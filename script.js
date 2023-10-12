59d8ecb5f40a05edd0e7c9dc04cfd44953d79354
reliable99 <abdulmuhminsodiq@gmail.com>
2023-10-12 11:06:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("arithmetic operator");
+
+// Arithmetic operators are used to perform mathematical operations between numeric operands.
+
+const a = 13;
+const b = 12;
+let result = 0;
+
+//addition
+// result = a + b;
+
+// substraction
+// result = a - b;
+
+//multiplication
+// result = a * b;
+
+// division
+// result = a / b;
+
+// exponent a to the power of b
+// result = a ** b;
+
+//modulo
+// result = 13 % 12;
+
+// increament / decreament operator: it simply increase the value of result to be one more than what its currently is
+
+// result++;
+result--;
+
+console.log(result);
